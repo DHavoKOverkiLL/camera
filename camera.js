4f808f8fc5bb704ccf1c299b9ac0ec93ce0a2a5b
Buia Vasile George <35294186+DHavoKOverkiLL@users.noreply.github.com>
2021-10-11 11:16:03 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,6 +1,5 @@
 var video = document.getElementById("video");
-video.addEventListener("touchstart",capteaza);
-video.addEventListener("mousedown",capteaza);
+
 function on_cam_success(stream)
 {
 	video.srcObject = stream;
@@ -21,3 +20,6 @@ function capteaza()
 	var ctx = c.getcontext("2d");
 	ctx.drawImage(video,0,0,640,480);
 }
+
+video.addEventListener("touchstart",capteaza);
+video.addEventListener("mousedown",capteaza);
